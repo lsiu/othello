935338caf192c239d8bc189a4a3cf8eb0d03508f
lsiu <leonard.siu@gmail.com>
2018-02-21 21:25:56 +0800

    extract methods from OthelloGame.move to make it easier to read

--- src/main/java/com/github/lsiu/othello/game/OthelloGame.java
+++ src/main/java/com/github/lsiu/othello/game/OthelloGame.java
@@ -23,19 +23,12 @@ public class OthelloGame {
     }
 
     public void move(String move) {
-        Location location = locationConverter.convert(move);
-        if (board.get(location) != LocationStatus.EMPTY) {
-            MoveDisplayUtils.throwExceptionOnInvalidMove(move, turn);
-            return;
-        }
+        Location location = convertLocation(move);
 
-        OthelloBoard othelloBoard = board.copy();
-        boolean piecesTurned = TurnSurroundingPiecesUtils.turnSurroundingPieces(othelloBoard, location, turn);
-        if (!piecesTurned) {
-            MoveDisplayUtils.throwExceptionOnInvalidMove(move, turn);
-        }
-        othelloBoard.mark(location, turn);
-        board = othelloBoard;
+        validateLocation(move, location);
+
+        putPieceAtLocationIfPossible(move, location);
+
         if (CanPlayMakeAMoveUtils.canPlayerMakeAMove(turn.opposite(), board)) {
             turn = turn.opposite();
         } else {
@@ -43,19 +36,49 @@ public class OthelloGame {
                 String moveOutput = MoveDisplayUtils.generateMoveOutput(move, turn, board);
                 throw new GameException(String.format("%s\n\nplayer %s cannot make a move, back to %s turn", moveOutput, turn.opposite(), turn));
             } else {
-                int xCount = CountPiecesUtils.count(board, LocationStatus.fromDisplayString("X"));
-                int oCount = CountPiecesUtils.count(board, LocationStatus.fromDisplayString("O"));
-                if (xCount > oCount) {
-                    MoveDisplayUtils.throwWinningMessage(LocationStatus.DARK, xCount, oCount, move, turn, board);
-                } else if (xCount == oCount) {
-                    String moveOutput = MoveDisplayUtils.generateMoveOutput(move, turn, board);
-                    throw new GameException(String.format("%s\n\n" +
-                            "No further moves available\n" +
-                            "Game tied ( %s vs %s )", moveOutput, xCount, oCount));
-                } else {
-                    MoveDisplayUtils.throwWinningMessage(LocationStatus.LIGHT, oCount, xCount, move, turn, board);
-                }
+                handleGameEnd(move);
             }
+        }
+    }
+
+    private void putPieceAtLocationIfPossible(String move, Location location) {
+        OthelloBoard othelloBoard = board.copy();
+        boolean piecesTurned = TurnSurroundingPiecesUtils.turnSurroundingPieces(othelloBoard, location, turn);
+        if (!piecesTurned) {
+            MoveDisplayUtils.throwExceptionOnInvalidMove(move, turn);
+        }
+        othelloBoard.mark(location, turn);
+        board = othelloBoard;
+    }
+
+    private void validateLocation(String move, Location location) {
+        if (board.get(location) != LocationStatus.EMPTY) {
+            MoveDisplayUtils.throwExceptionOnInvalidMove(move, turn);
+        }
+    }
+
+    private Location convertLocation(String move) {
+        Location location = null;
+        try {
+            location = locationConverter.convert(move);
+        } catch (Exception e) {
+            MoveDisplayUtils.throwExceptionOnInvalidMove(move, turn);
+        }
+        return location;
+    }
+
+    private void handleGameEnd(String move) {
+        int xCount = CountPiecesUtils.count(board, LocationStatus.fromDisplayString("X"));
+        int oCount = CountPiecesUtils.count(board, LocationStatus.fromDisplayString("O"));
+        if (xCount > oCount) {
+            MoveDisplayUtils.throwWinningMessage(LocationStatus.DARK, xCount, oCount, move, turn, board);
+        } else if (xCount == oCount) {
+            String moveOutput = MoveDisplayUtils.generateMoveOutput(move, turn, board);
+            throw new GameException(String.format("%s\n\n" +
+                    "No further moves available\n" +
+                    "Game tied ( %s vs %s )", moveOutput, xCount, oCount));
+        } else {
+            MoveDisplayUtils.throwWinningMessage(LocationStatus.LIGHT, oCount, xCount, move, turn, board);
         }
     }
 
