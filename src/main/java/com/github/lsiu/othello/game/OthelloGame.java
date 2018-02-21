d4741181896bad716e12da5fd81a551c85e4bf09
lsiu <leonard.siu@gmail.com>
2018-02-21 20:48:16 +0800

    add last move win and last move lose scenarios. Add tie game scenario

--- src/main/java/com/github/lsiu/othello/game/OthelloGame.java
+++ src/main/java/com/github/lsiu/othello/game/OthelloGame.java
@@ -43,12 +43,37 @@ public class OthelloGame {
                 String moveOutput = MoveDisplayUtils.generateMoveOutput(move, turn, board);
                 throw new GameException(String.format("%s\n\nplayer %s cannot make a move, back to %s turn", moveOutput, turn.opposite(), turn));
             } else {
-                String moveOutput = MoveDisplayUtils.generateMoveOutput(move, turn, board);
-                throw new GameException(String.format("%s\n\n" +
-                                "No further moves available\n" +
-                                "Player '%s' wins ( %s vs %s )", moveOutput, turn,
-                        CountPiecesUtils.count(board, turn), CountPiecesUtils.count(board, turn.opposite())));
+                int xCount = CountPiecesUtils.count(board, LocationStatus.fromDisplayString("X"));
+                int oCount = CountPiecesUtils.count(board, LocationStatus.fromDisplayString("O"));
+                if (xCount > oCount) {
+                    throwWinningMessage(LocationStatus.BLACK, xCount, oCount, move);
+                } else if (xCount == oCount) {
+                    String moveOutput = MoveDisplayUtils.generateMoveOutput(move, turn, board);
+                    throw new GameException(String.format("%s\n\n" +
+                            "No further moves available\n" +
+                            "Game tied ( %s vs %s )", moveOutput, xCount, oCount));
+                } else {
+                    throwWinningMessage(LocationStatus.WHITE, oCount, xCount, move);
+                }
             }
+        }
+    }
+
+    private void throwWinningMessage(LocationStatus winner, int winnerCount, int loserCount, String move) {
+        String moveOutput = MoveDisplayUtils.generateMoveOutput(move, turn, board);
+        throw new GameException(String.format("%s\n\n" +
+                        "No further moves available\n" +
+                        "Player '%s' wins ( %s vs %s )", moveOutput, winner,
+                winnerCount, loserCount));
+    }
+
+    private LocationStatus determineWinner(int xCount, int oCount) {
+        if (xCount > oCount) {
+            return LocationStatus.BLACK;
+        } else if (xCount == oCount) {
+            return null;
+        } else {
+            return LocationStatus.WHITE;
         }
     }
 
--- src/test/resources/com/github/lsiu/othello/features/PlayerCannotMakeMove.feature
+++ src/test/resources/com/github/lsiu/othello/features/PlayerCannotMakeMove.feature
@@ -64,4 +64,97 @@ Feature: Player cannot make move
     """
 
 
-  Scenario: Board is full. Game ends
+  Scenario: Game ends. Last move loses
+    Given this is the state of the game and is WHITE's turn
+    """
+    1 XXXXXXXX
+    2 XXXXXXOX
+    3 XXXXXXXX
+    4 XXXXXXXX
+    5 OXXXXXXX
+    6 OXXXXXXX
+    7 -XXOXXXX
+    8 OXXOXXXX
+      abcdefgh
+    """
+    When move a7 command entered
+    And expect game exception with message:
+    """
+    Player 'O' move: a7
+    1 XXXXXXXX
+    2 XXXXXXOX
+    3 XXXXXXXX
+    4 XXXXXXXX
+    5 OXXXXXXX
+    6 OXXXXXXX
+    7 OOOOXXXX
+    8 OXXOXXXX
+      abcdefgh
+
+    No further moves available
+    Player 'X' wins ( 55 vs 9 )
+    """
+
+
+  Scenario: Game ends. Last move wins
+    Given this is the state of the game and is BLACK's turn
+    """
+    1 XXXXXXXX
+    2 XXXXXXOX
+    3 XXXXXXXX
+    4 XXXXXXXX
+    5 OXXXXXXX
+    6 OXXXXXXX
+    7 -XXOXXXX
+    8 OXXOXXXX
+      abcdefgh
+    """
+    When move a7 command entered
+    And expect game exception with message:
+    """
+    Player 'X' move: a7
+    1 XXXXXXXX
+    2 XXXXXXOX
+    3 XXXXXXXX
+    4 XXXXXXXX
+    5 XXXXXXXX
+    6 XXXXXXXX
+    7 XXXOXXXX
+    8 OXXOXXXX
+      abcdefgh
+
+    No further moves available
+    Player 'X' wins ( 60 vs 4 )
+    """
+
+
+  Scenario: Game ends. Tie Game
+    Given this is the state of the game and is BLACK's turn
+    """
+    1 XOOOOOOO
+    2 XOOOOOOO
+    3 XOOOOOOO
+    4 XOOOOOOO
+    5 OXXXXXXX
+    6 OXXXXXXO
+    7 -XXOXXXX
+    8 OXXOXXXX
+      abcdefgh
+    """
+    When move a7 command entered
+    And expect game exception with message:
+    """
+    Player 'X' move: a7
+    1 XOOOOOOO
+    2 XOOOOOOO
+    3 XOOOOOOO
+    4 XOOOOOOO
+    5 XXXXXXXX
+    6 XXXXXXXO
+    7 XXXOXXXX
+    8 OXXOXXXX
+      abcdefgh
+
+    No further moves available
+    Game tied ( 32 vs 32 )
+    """
